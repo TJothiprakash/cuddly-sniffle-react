ed9f0156d6d4d2027ccd0d4fa77ba08bab144053
jothiprakash <jothiprakash888@gmail.com>
2024-10-26 13:55:06 +0530

    oct 26 finished Rating component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/randomcolor';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
     {/* <Accordian/> */}
     {/* random color componenet */}
-    <RandomColor/>
+    {/* <RandomColor/> */}
+    {/* Star rating component */}
+    <StarRating noOfStars={10} />
     </div>
   );
 }
